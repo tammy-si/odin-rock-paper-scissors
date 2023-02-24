4f2db282cbd44e9b7640e85b806060a7c612d528
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-02-24 14:36:04 -0800

    Done

--- myscript.js
+++ myscript.js
@@ -11,6 +11,49 @@ function getComputerChoice() {
     }
 }
 
-for (let i = 0; i < 5; i++){
-    console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection)
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") || 
+                (playerSelection == "paper" && computerSelection == "rock") ||
+                (playerSelection == "scissors" && computerSelection == "paper")) 
+                {
+        return "Player";
+    } else {
+        return "Computer";
+    }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Enter Choice: ").toLowerCase()
+        let computerSelection = getComputerChoice();
+
+        let winner = playRound(playerSelection, computerSelection)
+        if (winner == "Tie") {
+            console.log("Tie")
+        } else if (winner == "Player") {
+            console.log("You win " + playerSelection + " beats " + computerSelection)
+            playerScore += 1
+        }
+        else {
+            console.log("You lose " + computerSelection + " beats " + playerSelection)
+            computerScore += 1
+        }
+        console.log("Player: " + playerScore + ", Computer Score: " + computerScore)
+    }
+
+    // reports winner
+    if (playerScore == computerScore) {
+        console.log("Tie")
+    } else if (playerScore > computerScore) {
+        console.log("Player wins")
+    } else {
+        console.log("Computer wins")
+    }
+}
+
+game();
